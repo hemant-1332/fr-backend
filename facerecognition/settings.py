e74a980a9461ec2240051a43eaf6d74a90a09f45
Hemant Singh <striker.hemant@gmail.com>
2020-05-10 01:13:55 +0530

    updated

--- facerecognition/settings.py
+++ facerecognition/settings.py
@@ -131,6 +131,8 @@ PROJECT_ROOT = os.path.abspath(BASE_DIR)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),    
